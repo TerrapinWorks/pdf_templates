192ac78bfe4c267b6bf9180d70d281d6a9406798
Nicholas Bentley <njbentley1@gmail.com>
2018-04-18 16:15:01 -0400

    updates

--- tw-default.php
+++ tw-default.php
@@ -48,8 +48,8 @@ if ( ! class_exists( 'GFForms' ) ) {
     <table class="header">
     	<tr>
     		<td width="35%" style="background-color: #cf202e; text-align: center; font-size: 20pt; color: #ffffff;"><strong>INVOICE</strong></td>
-    		<td width="32.5%" style="text-align: right; font-size: 13px;">University of Maryland<br />Engineering Information Technology<br />4356 Stadium Dr. Suite 2125<br />College Park, MD 20742</td>
-    		<td width="32.5%" style="text-align: right;"><img width="200px" src="https://s3.amazonaws.com/cdn.terrapin.works/brand/terrapin-works-logo_final_red-black_RPC.png"></img></td>
+    		<td width="32.5%" style="text-align: left; font-size: 13px;">University of Maryland<br />Engineering Information Technology<br />4356 Stadium Dr. Suite 2125<br />College Park, MD 20742</td>
+    		<td width="32.5%" style="text-align: center;"><img width="200px" src="https://s3.amazonaws.com/cdn.terrapin.works/brand/terrapin-works-logo_final_red-black_RPC.png"></img></td>
     	</tr>
     </table>
 </htmlpageheader>
