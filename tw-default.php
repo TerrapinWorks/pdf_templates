fb904197353c38f978fefc5fdc36c3a525bc3a7e
Nicholas Bentley <njbentley1@gmail.com>
2018-04-18 15:36:54 -0400

    updates

--- tw-default.php
+++ tw-default.php
@@ -46,7 +46,7 @@ if ( ! class_exists( 'GFForms' ) ) {
     <table class="header">
     	<tr>
     		<td width="40%" style="background-color: #cf202e; text-align: center; font-size: 20pt; color: #ffffff;"><strong>INVOICE</strong></td>
-    		<td width="30%" style="text-align: center;">University of Maryland<br />Terrapin Works System<br />4356 Stadium Dr. Suite 2125<br />College Park, MD 20742</td>
+    		<td width="30%" style="text-align: center;">University of Maryland<br />Engineering Information Technology<br />4356 Stadium Dr. Suite 2125<br />College Park, MD 20742</td>
     		<td width="30%" style="text-align: center;"><img width="150px" src="https://s3.amazonaws.com/cdn.terrapin.works/brand/terrapin-works-logo_final_red-black_RPC.png"></img></td>
     	</tr>
     </table>
