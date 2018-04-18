a3806e7c275ab41e2f40f0090644c503227805f9
Nicholas Bentley <njbentley1@gmail.com>
2018-04-18 15:02:18 -0400

    push first test template

--- tw-default.php
+++ tw-default.php
@@ -3,7 +3,7 @@
  * Template Name: Terrapin Works Default Invoice Template
  * Version: 0.1
  * Description: The default Terrapin Works invoice template. Not configured for any payment methods.
- * Author: Nicholas Bentley, Mterus IT Services
+ * Author: Nicholas Bentley, Merus IT Services
  * Author URI: https://merus.it
  * Group: Terrapin Works Templates
  * License: GPLv2
@@ -16,11 +16,56 @@ if ( ! class_exists( 'GFForms' ) ) {
 }
 ?>
 
-<!-- Any PDF CSS styles can be placed in the style tag below -->
+<!-- Any PDF CSS styles can be placed in here -->
 <style>
-	@page {
-        header: html_TerrapinWorksDefaultHeader; /* display <htmlpageheader name="TerrapinWorksDefaultHeader"> on all pages */
-        footer: html_MyCustomFooter; /* display <htmlpagefooter name="MyCustomFooter"> on all pages */
+    @page {
+        header: html_twHeader;
+        footer: html_twFooter;
+    }
+    table.header {
+    	border-bottom: 1px solid #000000; 
+    	vertical-align: bottom; 
+    	font-family: serif; 
+    	font-size: 9pt; 
+    	color: #000088;
+    	width: 100%;
+    }
+    .footer {
+    	text-align: center;
     }
 </style>
+
+
+<!-- register header and footer -->
+<htmlpageheader name="twHeader">
+    <table class="header">
+    	<tr>
+    		<td width="50%">Main Header</td>
+    		<td width="50%" style="text-align: right;">Logo</td>
+    	</tr>
+    </table>
+</htmlpageheader>
+
+<htmlpagefooter name="twFooter">
+    <div class="footer">
+    	{PAGENO}/{nbpg}
+    </div>
+</htmlpagefooter>
+
+<!-- Write PDF body -->
+<h2>Header</h2>
+    
+<p>Nulla felis erat, imperdiet eu, ullamcorper non, nonummy quis, elit. Suspendisse potenti. Ut a eros at ligula vehicula pretium. Maecenas feugiat pede vel risus. Nulla et lectus. Fusce eleifend neque sit amet erat. Integer consectetuer nulla non orci. Morbi feugiat pulvinar dolor. Cras odio. Donec mattis, nisi id euismod auctor, neque metus pellentesque risus, at eleifend lacus sapien et risus. Phasellus metus. Phasellus feugiat, lectus ac aliquam molestie, leo lacus tincidunt turpis, vel aliquam quam odio et sapien. Mauris ante pede, auctor ac, suscipit quis, malesuada sed, nulla. Integer sit amet odio sit amet lectus luctus euismod. Donec et nulla. Sed quis orci. </p>
+
+<pagebreak />
+
+<h2>Header</h2>
+
+<p>Nulla felis erat, imperdiet eu, ullamcorper non, nonummy quis, elit. Suspendisse potenti. Ut a eros at ligula vehicula pretium. Maecenas feugiat pede vel risus. Nulla et lectus. Fusce eleifend neque sit amet erat. Integer consectetuer nulla non orci. Morbi feugiat pulvinar dolor. Cras odio. Donec mattis, nisi id euismod auctor, neque metus pellentesque risus, at eleifend lacus sapien et risus. Phasellus metus. Phasellus feugiat, lectus ac aliquam molestie, leo lacus tincidunt turpis, vel aliquam quam odio et sapien. Mauris ante pede, auctor ac, suscipit quis, malesuada sed, nulla. Integer sit amet odio sit amet lectus luctus euismod. Donec et nulla. Sed quis orci. </p>
+
+<pagebreak />
+
+<h2>Header</h2>
+
+<p>Nulla felis erat, imperdiet eu, ullamcorper non, nonummy quis, elit. Suspendisse potenti. Ut a eros at ligula vehicula pretium. Maecenas feugiat pede vel risus. Nulla et lectus. Fusce eleifend neque sit amet erat. Integer consectetuer nulla non orci. Morbi feugiat pulvinar dolor. Cras odio. Donec mattis, nisi id euismod auctor, neque metus pellentesque risus, at eleifend lacus sapien et risus. Phasellus metus. Phasellus feugiat, lectus ac aliquam molestie, leo lacus tincidunt turpis, vel aliquam quam odio et sapien. Mauris ante pede, auctor ac, suscipit quis, malesuada sed, nulla. Integer sit amet odio sit amet lectus luctus euismod. Donec et nulla. Sed quis orci. </p>
 
