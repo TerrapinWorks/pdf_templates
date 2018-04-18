f00af83c17e448f2372e6b52b84b76d9797c61e0
Nicholas Bentley <njbentley1@gmail.com>
2018-04-18 14:41:51 -0400

    push first test template

---
+++ tw-default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Terrapin Works Default Invoice Template
+ * Version: 0.1
+ * Description: The default Terrapin Works invoice template. Not configured for any payment methods.
+ * Author: Nicholas Bentley, Mterus IT Services
+ * Author URI: https://merus.it
+ * License: GPLv2
+ * Required PDF Version: 4.0
+ * Tags: terrapinworks, invoice
+ */
+/* Prevent direct access to the template (always good to include this) */
+if ( ! class_exists( 'GFForms' ) ) {
+	return;
+}
+?>
+
+<!-- Any PDF CSS styles can be placed in the style tag below -->
+<style>
+</style>
+
+<h1>Sup boi</h1>
