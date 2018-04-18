eef82492c26ebedccd53c7631c3e395b43e72ecf
Nicholas Bentley <njbentley1@gmail.com>
2018-04-18 18:20:37 -0400

    updates

--- tw-default.php
+++ tw-default.php
@@ -137,6 +137,25 @@ if ( ! class_exists( 'GFForms' ) ) {
 <div class="row">
     <div class="col col-1-3">
         <div class="inner" height="40mm">
+            <div><h3 class="section1_header">Billed To</h3></div><div>John Doe</div><div>1 Address Way</div><div>City, State Zip</div>
+        </div>
+    </div>
+
+    <div class="col col-1-3">
+        <div class="inner" height="40mm">
+            <div><h3 class="section1_header">Invoice Number</h3></div><div class="bottompadding">TWINV2018-0001</div><h3 class="section1_header">Date Issued</h3><div>Current Date</div>
+        </div>
+    </div>
+
+    <div class="col col-1-3">
+        <div class="inner" height="40mm">
+           <div><h3 class="section1_header">Invoice Total</h3></div><div class="bottompadding_2"><h1>$55.12</h1></div><div><h3 class="section1_header">Payment Status</h3></div><div>Paid Status</div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col col-1-3">
+        <div class="inner" height="40mm">
             <div><h3 class="section1_header">Description</h3></div><div>Payment Description</div>
     </div>
 
